159aefce5626e6b31cb6500eeb1de1562640f08b
Eyal Paz <eyal@ox.security>
2024-05-06 11:33:34 +0300

    add hardcoded search

--- flask_webgoat/ui.py
+++ flask_webgoat/ui.py
@@ -22,3 +22,20 @@ def search():
     except sqlite3.Error as err:
         message = "Error while executing query " + query_param + ": " + err
         return render_template("error.html", message=message)
+
+@bp.route("/hardcoded-search")
+def search():
+    query_param = 'Adele'
+    if query_param is None:
+        message = "please provide the query parameter"
+        return render_template("error.html", message=message)
+
+    try:
+        query = "SELECT username, access_level FROM user WHERE username LIKE ?;"
+        results = query_db(query, (query_param,))
+        return render_template(
+            "search.html", results=results, num_results=len(results), query=query_param
+        )
+    except sqlite3.Error as err:
+        message = "Error while executing query " + query_param + ": " + err
+        return render_template("error.html", message=message)
